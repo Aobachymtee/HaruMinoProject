8857d420e7e035af7a17406e25a44b9ee5bfefb9
AobaVN <62795306+Aobachymtee@users.noreply.github.com>
2023-05-24 01:17:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,5 +34,38 @@ creatorLink.addEventListener("click", function() {
 
 });
 
+// Popup
+
+var popup = document.getElementById("popup");
+
+var popupLink = document.getElementById("popup-link");
+
+var closePopup = document.getElementsByClassName("close")[0];
+
+popupLink.addEventListener("click", function(event) {
+
+  event.preventDefault(); // Prevent the default link behavior
+
+  popup.style.display = "block";
+
+});
+
+closePopup.addEventListener("click", function() {
+
+  popup.style.display = "none";
+
+});
+
+window.addEventListener("click", function(event) {
+
+  if (event.target == popup) {
+
+    popup.style.display = "none";
+
+  }
+
+});
+
+// Rest of your existing JavaScript code
 
 
